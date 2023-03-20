394765a3e4987e1ec279c7147baf7445a6add8af
Jiyeon_Hong <hong219@wemade.com>
2023-03-20 10:42:21 +0900

    modify log

--- aws_waf/log_groups.tf
+++ aws_waf/log_groups.tf
@@ -1,9 +1,9 @@
 resource "aws_cloudwatch_log_group" "this" {
-  name = format("aws-waf-logs-%s%s-%s", var.prefix, var.env, var.purpose)
+  name = format("aws-waf-logs-%s", var.env)
 
   retention_in_days = var.logging_retention_in_days
 
   tags = merge(local.default_tags, {
-    Name = format("aws-waf-logs-%s%s-%s", var.prefix, var.env, var.purpose)
+    Name = format("aws-waf-logs-%s", var.env)
   })
 }
